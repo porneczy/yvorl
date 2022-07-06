5a4442f4a4628dba22f26dc167823d11257c94c9
porneczy <porneczy@gmail.com>
2022-07-06 19:21:34 +0200

    addPariclesAnimatedBackground

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import ParticlesBackGround from './components/ParticlesBackGround';
 
 function App() {
   return (
     <div className="App">
-
+    <ParticlesBackGround /> 
     </div>
   );
 }
---
+++ src/components/ParticlesBackGround.jsx
@@ -0,0 +1,155 @@
+import React from 'react'
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+
+function ParticlesBackGround() {
+
+    const particlesInit = async (main) => {
+        console.log(main);
+
+        // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+        // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+        // starting from v2 you can add only the features you need reducing the bundle size
+        await loadFull(main);
+    };
+
+    return (
+        <div>
+            <Particles
+                id="tsparticles"
+                init={particlesInit}
+
+                options={{
+                    "fullScreen": {
+                        "enable": true,
+                        "zIndex": 1
+                    },
+                    "particles": {
+                        "number": {
+                            "value": 100,
+                            "density": {
+                                "enable": true,
+                                "value_area": 800
+                            }
+                        },
+                        "color": {
+                            "value": "#fff"
+                        },
+                        "shape": {
+                            "type": "circle",
+                            "stroke": {
+                                "width": 0,
+                                "color": "#000000"
+                            },
+                            "polygon": {
+                                "nb_sides": 5
+                            },
+                            "image": {
+                                "src": "img/github.svg",
+                                "width": 100,
+                                "height": 100
+                            }
+                        },
+                        "opacity": {
+                            "value": 0.4,
+                            "random": true,
+                            "anim": {
+                                "enable": false,
+                                "speed": 1,
+                                "opacity_min": 0.1,
+                                "sync": false
+                            }
+                        },
+                        "size": {
+                            "value": 3,
+                            "random": true,
+                            "anim": {
+                                "enable": false,
+                                "speed": 40,
+                                "size_min": 0.1,
+                                "sync": false
+                            }
+                        },
+                        "rotate": {
+                            "value": 0,
+                            "random": true,
+                            "direction": "clockwise",
+                            "animation": {
+                                "enable": true,
+                                "speed": 5,
+                                "sync": false
+                            }
+                        },
+                        "line_linked": {
+                            "enable": true,
+                            "distance": 150,
+                            "color": "#ffffff",
+                            "opacity": 0.4,
+                            "width": 1
+                        },
+                        "move": {
+                            "enable": true,
+                            "speed": 2,
+                            "direction": "none",
+                            "random": false,
+                            "straight": false,
+                            "out_mode": "out",
+                            "attract": {
+                                "enable": false,
+                                "rotateX": 600,
+                                "rotateY": 1200
+                            }
+                        }
+                    },
+                    "interactivity": {
+                        "events": {
+                            "onhover": {
+                                "enable": true,
+                                "mode": ["grab"]
+                            },
+                            "onclick": {
+                                "enable": true,
+                                "mode": "push"
+                            },
+                            "resize": true
+                        },
+                        "modes": {
+                            "grab": {
+                                "distance": 130,
+                                "line_linked": {
+                                    "opacity": 1
+                                }
+                            },
+                            "bubble": {
+                                "distance": 400,
+                                "size": 40,
+                                "duration": 2,
+                                "opacity": 8,
+                                "speed": 3
+                            },
+                            "repulse": {
+                                "distance": 200
+                            },
+                            "push": {
+                                "particles_nb": 4
+                            },
+                            "remove": {
+                                "particles_nb": 2
+                            }
+                        }
+                    },
+                    "retina_detect": true,
+                    "background": {
+                        "color": "#111",
+                        "image": "",
+                        "position": "50% 50%",
+                        "repeat": "no-repeat",
+                        "size": "cover"
+                    }
+                }}
+            />
+        </div>
+    )
+}
+
+export default ParticlesBackGround
